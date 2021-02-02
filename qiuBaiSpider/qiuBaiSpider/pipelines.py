2f12e3a499781fe83b07aec54eabec7b3e391609
black-mesa-canteen <shaotienlee@foxmail.com>
2021-02-02 16:26:49 +0800

    learned scrapy pipeline

--- qiuBaiSpider/qiuBaiSpider/pipelines.py
+++ qiuBaiSpider/qiuBaiSpider/pipelines.py
@@ -5,9 +5,46 @@
 
 
 # useful for handling different item types with a single interface
+import pymysql as pymysql
 from itemadapter import ItemAdapter
 
 
-class QiubaispiderPipeline:
+class QiubaispiderPipeline(object):
+    fp = None
+    def open_spider(self):
+        self.fp = open('./qiubai.txt', 'w', encoding='utf-8')
+    # used to handle Item objects
+    # every time it yields a item, it will work for once
     def process_item(self, item, spider):
+        author = item['author']
+        content = item['content']
+
+        self.fp.write(author + ': ' + content + '\n')
+
         return item
+
+    def close_spider(self):
+        self.fp.close()
+
+class MysqlPipeLine(object):
+    conn = None
+    cursor = None
+
+    def open_spider(self, spider):
+        self.conn = pymysql.Connect(host='127.0.0.1', port=3306, user='root', password='', db='unichat', charset='utf8')
+
+    def process_item(self, item, spider):
+        self.cursor = self.conn.cursor()
+
+        try:
+            self.cursor.execute('INSERT INTO unichat.qiubai (name, content) VALUES ("%s", "%s") ' % (item["author"], item["content"]))
+            self.conn.commit()
+        except Exception as e:
+            print(e)
+            self.conn.rollback()
+
+        return item
+
+    def close_spider(self, spider):
+        self.cursor.close()
+        self.conn.close()
--- qiuBaiSpider/qiuBaiSpider/spiders/qiubai.py
+++ qiuBaiSpider/qiuBaiSpider/spiders/qiubai.py
@@ -1,21 +1,52 @@
 import scrapy
 
+from qiuBaiSpider.qiuBaiSpider.items import QiubaispiderItem
+
 
 class QiubaiSpider(scrapy.Spider):
     name = 'qiubai'
     allowed_domains = ['www.qiushibaike.com']
     start_urls = ['https://www.qiushibaike.com/text/']
 
+    # def parse(self, response):
+    #     div_list = response.xpath('//*[@id="content"]/div/div[2]/div')
+    #     all_data = []
+    #     for div in div_list:
+    #
+    #         # will return selector object list, but we need text!! Use extract() method
+    #         author = div.xpath('./div[@class="author clearfix"]/a[1]/img/@alt').extract()[0]
+    #         content_list = div.xpath('./a[@class="contentHerf"]/div/span//text()').extract()
+    #         # res_str = ''
+    #         # for str in content_list:
+    #         #     res_str += str
+    #         res_str = ''.join(content_list)
+    #         print(author, ' 说：', res_str)
+    #
+    #         dic = {
+    #             'author': author,
+    #             'content': res_str
+    #         }
+    #         all_data.append(dic)
+    #
+    #     return all_data
+
     def parse(self, response):
         div_list = response.xpath('//*[@id="content"]/div/div[2]/div')
+        all_data = []
         for div in div_list:
 
             # will return selector object list, but we need text!! Use extract() method
             author = div.xpath('./div[@class="author clearfix"]/a[1]/img/@alt').extract()[0]
             content_list = div.xpath('./a[@class="contentHerf"]/div/span//text()').extract()
-            res_str = ''
-            for str in content_list:
-                res_str += str
-            print(author, ' 说：', res_str)
-            break
+            # res_str = ''
+            # for str in content_list:
+            #     res_str += str
+            res_str = ''.join(content_list)
+
+            item = QiubaispiderItem()
+            item['author'] = author
+            item['content'] = res_str
+
+            yield item
+
 
